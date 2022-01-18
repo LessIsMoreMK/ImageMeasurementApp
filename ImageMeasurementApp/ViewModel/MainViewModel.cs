4b02cf86590e7d2c1385f552d1da6ddf1df560b3
LessIsMoreMK <maciej8kz@gmail.com>
2022-01-18 11:38:41 +0100

    Basic MVVM setup, LoadImage

---
+++ ImageMeasurementApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ImageMeasurementApp
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Private Members
+
+        private string mImagePath;
+
+        #endregion
+
+        #region Properties
+
+        public string ImagePath
+        {
+            get => mImagePath;
+            set { mImagePath = value; }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand LoadImageCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public MainViewModel()
+        {
+            LoadImageCommand = new RelayCommand(LoadImage);
+        }
+        #endregion
+
+        #region Methods
+
+        private void LoadImage()
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.DefaultExt = (".png");
+            open.Filter = "Pictures (*.jpg;*.gif;*.png)|*.jpg;*.gif;*.png";
+
+            if (open.ShowDialog() == true)
+                ImagePath = open.FileName;
+            var a = 1;
+        }
+
+        #endregion
+    }
+}
