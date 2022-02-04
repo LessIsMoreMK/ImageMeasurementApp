d921d4160fc27a02262dcf3e32597d83d30ec1f7
LessIsMoreMK <maciej8kz@gmail.com>
2022-02-04 11:23:26 +0100

    Image conversions methods And MainWidnow Layout

--- ImageMeasurementApp/Helpers/BaseViewModel.cs
+++ ImageMeasurementApp/Helpers/BaseViewModel.cs
@@ -1,7 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using System;
 
 namespace ImageMeasurementApp
 {
