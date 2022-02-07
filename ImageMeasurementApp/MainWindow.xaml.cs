adacf39bb4a6b76928516bd047f5f54cfaf21e64
LessIsMoreMK <maciej8kz@gmail.com>
2022-02-07 22:37:33 +0100

    Custom window style

--- ImageMeasurementApp/MainWindow.xaml.cs
+++ ImageMeasurementApp/MainWindow.xaml.cs
@@ -11,7 +11,7 @@ namespace ImageMeasurementApp
         {
             InitializeComponent();
 
-            DataContext = new MainViewModel();
+            DataContext = new WindowViewModel(this);
         }
     }
 }
---
+++ ImageMeasurementApp/ValueConverters/BooleanToBorderThicknessConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ImageMeasurementApp
+{
+    /// <summary>
+    /// A converter that takes in a boolean and returns a thickness of 2 if true, useful for applying 
+    /// border radius on a true value
+    /// </summary>
+    public class BooleanToBorderThicknessConverter : BaseValueConverter<BooleanToBorderThicknessConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null)
+                return (bool)value ? 1 : 0;
+            else
+                return (bool)value ? 0 : 1;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
